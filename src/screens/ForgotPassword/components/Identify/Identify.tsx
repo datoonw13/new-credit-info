8e29e700cfc0a25f93016dd2d06b5ac8f54cf341
giunashvili <giorgi.official@yahoo.com>
2021-03-11 10:41:04 +0400

    chore: working on forgot password

--- src/screens/ForgotPassword/components/Identify/Identify.tsx
+++ src/screens/ForgotPassword/components/Identify/Identify.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
-import {Text} from 'components';
+import {StyleSheet, View} from 'react-native';
+import {Button, Input, SendAgain} from 'components';
+import {useTranslation} from 'react-i18next';
+import {Divider} from 'react-native-elements';
 
 const Identify = () => {
-  return Text({children: 'Identify'});
+  const {t} = useTranslation();
+  return (
+    <View>
+      <Input
+        autoCapitalize="none"
+        placeholder="username"
+        onBlur={() => {}}
+        onChangeText={() => {}}
+        value={''}
+        keyboardType="number-pad"
+        errorMessage={'' && t('authorization.validUsername')}
+        label={'user'}
+        maxLength={11}
+        labelOnBorderToo
+      />
+      <Divider />
+      <Input
+        onBlur={() => {}}
+        onChangeText={() => {}}
+        value={''}
+        maxLength={6}
+        keyboardType="number-pad"
+        label={'registration.OTP'}
+        errorMessage={'' && t('registration.validOTP')}
+      />
+      <SendAgain phoneNumber={''} />
+
+      <Button touchableStyle={styles.button} text="continue" />
+    </View>
+  );
 };
 
 export default Identify;
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 20,
+  },
+});
--- src/screens/ForgotPassword/components/Reset/Reset.tsx
+++ src/screens/ForgotPassword/components/Reset/Reset.tsx
@@ -1,8 +1,63 @@
 import React from 'react';
-import {Text} from 'components';
+import {View, StyleSheet} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {colors} from 'theme';
+import {Input, Button} from 'components';
+import {Divider} from 'react-native-elements';
 
 const Reset = () => {
-  return Text({children: 'Reset'});
+  return (
+    <View>
+      <Input
+        onBlur={() => {}}
+        value={''}
+        maxLength={35}
+        label="registration.password"
+        secureTextEntry={true}
+        // rightIconPressHandler={() => {
+        //   setPasswordVisible(!passwordVisible);
+        // }}
+        errorMessage={''}
+        onChangeText={(val) => {}}
+        rightIcon={
+          <Ionicons
+            name={true ? 'eye-off' : 'eye'}
+            color={colors.GRAY8}
+            size={22}
+          />
+        }
+      />
+
+      <Divider />
+
+      <Input
+        onBlur={() => {}}
+        value={''}
+        maxLength={35}
+        label="registration.repeatPassword"
+        secureTextEntry={true}
+        // rightIconPressHandler={() =>
+        //   setRepeatPasswordVisible(!repeatPasswordVisible)
+        // }
+        onChangeText={() => {}}
+        rightIcon={
+          <Ionicons
+            name={true ? 'eye-off' : 'eye'}
+            color={colors.GRAY8}
+            size={22}
+          />
+        }
+        // errorMessage={errors.repeatPassword?.message}
+      />
+      <Button touchableStyle={styles.button} text="continue" />
+    </View>
+  );
 };
 
 export default Reset;
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 20,
+  },
+});
